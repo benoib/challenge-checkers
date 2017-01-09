668e397cfbba0ad45a78c7eaf9c40014a0e238be
Leo Paillier <leo.paillier@gmail.com>
2017-01-09 14:32:07 +0100

    Final update + output

--- ai.py
+++ ai.py
@@ -2,9 +2,9 @@ import random
 
 ###############################################################################
 
-def _get_player_discs(board, color):
+def _get_color_discs(board, color):
     """
-        Find all the discs belonging to the next player.
+        Find all the discs of the given color.
 
         Arguments:
         - board: the content of the board
@@ -178,62 +178,22 @@ def _update_board_move(board, moves):
 
 ###############################################################################
 
-def allowed_moves(board, color):
+def allowed_moves(board, color, all_moves=True):
     """
-        This is the first function you need to implement.
+        Compute either all allowed moves or only the capturing moves
 
         Arguments:
-        - board: The content of the board, represented as a list of strings.
-                 The length of strings are the same as the length of the list,
-                 which represents a NxN checkers board.
-                 Each string is a row, from the top row (the black side) to the
-                 bottom row (white side). The string are made of five possible
-                 characters:
-                 - '_' : an empty square
-                 - 'b' : a square with a black disc
-                 - 'B' : a square with a black king
-                 - 'w' : a square with a white disc
-                 - 'W' : a square with a white king
-                 At the beginning of the game:
-                 - the top left square of a board is always empty
-                 - the square on it right always contains a black disc
-        - color: the next player's color. It can be either 'b' for black or 'w'
-                 for white.
+        - board: the content of the board
+        - color: the next player's color
+        - all_moves: boolean representing whether we should return all possible
+                 moves (True, by default) or only the capturing moves (False)
 
         Return value:
-        It must return a list of all the valid moves. Please refer to the
-        README for a description of what are valid moves. A move is a list of
-        all the squares visited by a disc or a king, from its initial position
-        to its final position. The coordinates of the square must be specified
-        using (row, column), with both 'row' and 'column' starting from 0 at
-        the top left corner of the board (black side).
-
-        Example:
-        >> board = [
-            '________',
-            '__b_____',
-            '_w_w____',
-            '________',
-            '_w______',
-            '_____b__',
-            '____w___',
-            '___w____'
-        ]
-
-        The top-most black disc can chain two jumps and eat both left white
-        discs or jump only over the right white disc. The other black disc
-        cannot move because it does produces any capturing move.
-
-        The output must thus be:
-        >> allowed_moves(board, 'b')
-        [
-            [(1, 2), (3, 0), (5, 2)],
-            [(1, 2), (3, 4)]
-        ]
+        - moves: list of all the valid moves
     """
 
     # Retrieve next player discs position
-    discs_pos = _get_player_discs(board, color)
+    discs_pos = _get_color_discs(board, color)
 
     # Compute the possible moves
     moves_non_capt = []
@@ -264,77 +224,161 @@ def allowed_moves(board, color):
                     mv_queue += [pos_steps + [p] for p in pos_capt]
 
         # Retrieve non capturing positions
-        if moves_capt == []:
+        if moves_capt == [] and all_moves:
             pos_non_capt = _next_non_capt(board, disc_pos)
             moves_non_capt += [[disc_pos, p] for p in pos_non_capt]
 
-    return moves_non_capt if moves_capt == [] else moves_capt
+    # Update the final moves
+    if all_moves:
+        moves = moves_non_capt if moves_capt == [] else moves_capt
+    else:
+        moves = moves_capt
+
+    return moves
 
 ###############################################################################
 
-def _eval_board_single_color(board, color):
+def _get_unprotected_score(board, color):
     """
-        Evaluate the board score for one color individually.
-        The evaluation takes into consideration:
-            - the number of normal discs
-            - the number of kings
-            - the number of discs on sides
+        Compute the number of unprotected disks of the given color considering
+        that a disk is worth one point if let completely unprotected and a half
+        otherwise.
 
         Arguments:
         - board: the content of the board
-        - color: the color to be evaluated
+        - color: the color of the player
 
-        Return value:
-        - score: the board value, higher is better
+        Retur value:
+        - score: score corresponding to the unprotected discs
     """
-    # Values used for positions
-    KING_VAL = 4
-    DISC_VAL = 1
-    SIDE_VAL = 0.75
+    # Retrieve discs position
+    discs_pos = _get_color_discs(board, color)
 
-    # Bonus point for kings and discs
-    kings = 0
-    discs = 0
-    for line in board:
-        kings += line.count(color.upper())
-        discs += line.count(color)
-    score = KING_VAL * kings + DISC_VAL * discs
+    # Define forward direction and back row
+    bck_row = 0 if color == "b" else 7
+    fwd_dir = 1 if color == "b" else -1
 
-    # # Bonus points for side position
-    # sides = 0
-    # for line in board:
-    #     s = list(line)
-    #     sides += 1 if s[0] == color else 0
-    #     sides += 1 if s[-1] == color else 0
-    # score += SIDE_VAL * sides
+    # Loop through all positions
+    score = 0
+    for disc_pos in discs_pos:
+        row, col = disc_pos
+
+        # Discs in the back row are automatically protected
+        if row == bck_row or board[row][col] == color.upper():
+            continue
+
+        # Check left then right
+        score += 0.5 if col > 0 and board[row - fwd_dir][col - 1] == "_" else 0
+        score += 0.5 if col < 7 and board[row - fwd_dir][col + 1] == "_" else 0
 
     return score
+
 
 ###############################################################################
 
 def _eval_board(board, our_color):
     """
-        Heuristic used to evaluate a board.
+        Heuristic used to evaluate a board. As we don't have access to the
+        ennemy AI we will simply use their number of discs are metrics. For our
+        AI on the other hand we will use:
+            - number of kings and discs
+            - king centering
+            - disc protection
 
         Arguments:
         - board: the content of the board
-        - color: the color of our AI
+        - our_color: the color of our AI
 
         Return value:
         - score: the board value, higher is better
     """
     their_color = "b" if our_color == "w" else "w"
-    our_score = _eval_board_single_color(board, our_color)
-    their_score = _eval_board_single_color(board, their_color)
+
+    # Values used for positions
+    DISC_VAL = 1        # Value of a normal disc
+    KING_VAL = 3        # King
+    CNTR_VAL = 0.01     # King in the midle
+    UNPD_VAL = 0.5      # Unprotected score
+
+    # Initialize variables
+    our_kings   = 0;            their_kings = 0
+    our_discs   = 0;            their_discs = 0
+    middle_king = 0
+    unprotected = _get_unprotected_score(board, our_color)
+
+    # Go through all lines from our back to our front
+    for row in range(0, 8):
+        line = board[row]
+
+        # Number of kings and normal discs
+        our_kings += line.count(our_color.upper())
+        our_discs += line.count(our_color)
+        their_kings += line.count(their_color.upper())
+        their_discs += line.count(their_color)
+
+        # Number of kings on the midle
+        middle_king += line.count(our_color.upper()) * \
+            (3.5 - abs(row - 3.5))
+
+    # Update the scores
+    our_score = our_kings * KING_VAL + our_discs * DISC_VAL +\
+        middle_king * CNTR_VAL + unprotected * UNPD_VAL
+    their_score = their_kings * KING_VAL + their_discs * DISC_VAL
 
     return our_score - their_score
+
+###############################################################################
+
+def _last_eval_board(board, our_color, player_color):
+    """
+        Evaluate a board considering it's the last stage of the search tree.
+        Only capturing moves are evaluated until there are no more unresolved
+        capturing conflicts.
+
+        Arguments:
+        - board:        the content of the board
+        - our_color:    the color of our AI
+        - player_color: the color of the next player
+
+        Return value:
+        - best_score:   the score corresponding to the move that maximizes (or
+                        minimizes) the score depending on the player color
+    """
+    # Initialize variables
+    their_color = "w" if our_color == "b" else "b"
+    next_player_color = "w" if player_color == "b" else "b"
+
+    next_moves = allowed_moves(board, player_color, False)
+    if next_moves == []:
+        return _eval_board(board, our_color)
+    else:
+        # Initialize variables
+        best_score = None
+
+        # Go through all moves and recursively find the best
+        for next_move in next_moves:
+            # Update the board
+            new_board = _update_board_move(board, next_move)
+
+            # Compute best move and score
+            score = _last_eval_board(new_board, our_color, next_player_color)
+
+            # Update the score depending on the player color
+            if best_score == None:
+                best_score = score
+            elif player_color == our_color and score > best_score:
+                best_score = score
+            elif player_color == their_color and score < best_score:
+                best_score = score
+
+    return best_score
 
 ###############################################################################
 
 def _find_best_move(board, our_color, depth):
     """
         Recursively find the best move by maxmimzing the score with our move and
-        minimizing it with their score.
+        minimizing it with their move.
 
         Arguments:
         - board:        the content of the board
@@ -343,12 +387,12 @@ def _find_best_move(board, our_color, depth):
 
         Return value:
         - our_bst_mv:   our move to maximize score
-        - our_bst_scr:  correspond score
+        - our_bst_scr:  corresponding score
     """
     # Initialize variables
     their_color = "b" if our_color == "w" else "w"
     our_bst_mv = []
-    our_bst_scr = 0
+    our_bst_scr = _eval_board(board, our_color)
 
     # Go through all possible combination of our move and their move
     our_moves = allowed_moves(board, our_color)
@@ -358,7 +402,7 @@ def _find_best_move(board, our_color, depth):
 
         # Initialize variables
         their_bst_mv = []
-        their_bst_scr = 0
+        their_bst_scr = _eval_board(new_board1, our_color)
 
         # Go through each independent move
         for their_move in their_moves:
@@ -368,27 +412,17 @@ def _find_best_move(board, our_color, depth):
             if depth > 0:
                 _, score = _find_best_move(new_board2, our_color, depth - 1)
             else:
-                score = _eval_board(new_board2, our_color)
-
-            # Initial update if it was never assigned
-            if their_bst_mv == []:
-                their_bst_scr = score
-                their_bst_mv = their_move
+                score = _last_eval_board(new_board2, our_color, our_color)
 
             # Check if they can play better which means decreasing the score
-            if score < their_bst_scr:
+            if their_bst_mv == [] or score < their_bst_scr:
                 their_bst_scr = score
                 their_bst_mv = their_move
 
-        # Initial update if it was never assigned
-        if our_bst_mv == []:
-            our_bst_scr = their_bst_scr
-            our_bst_mv = our_move
-
         # Check if we can play better which means increasing the score
-        if their_bst_scr > our_bst_scr:
-            our_bst_mv = our_move
+        if our_bst_mv == [] or their_bst_scr > our_bst_scr:
             our_bst_scr = their_bst_scr
+            our_bst_mv = our_move
 
     return our_bst_mv, our_bst_scr
 
@@ -397,10 +431,8 @@ def _find_best_move(board, our_color, depth):
 def _number_disc(board):
     """
         Count the number of discs in the board.
-
         Arguments:
         - board: the content of the board
-
         Return value:
         - n: number of discs
     """
@@ -415,27 +447,14 @@ def _number_disc(board):
 
 def play(board, color):
     """
-        We look all the possible n moves in the future and considering that we
-        play perfectly and so does the other player we chose the move that
-        maximize our score using the _eval_board() function.
+        We look all the possible moves in the future up to a certain depth and
+        considering that we play perfectly and so does the other player we chose
+        the move that maximize our score defined using _eval_board()
     """
-    n_disc = _number_disc(board)
-    depth = 2 if n_disc > 15 else 3
+    # Define the depth of the tree
+    depth = 1 if _number_disc(board) > 6 else 2
 
-    print("Adaptative depth: %d" % depth)
-    best_move, best_score = _find_best_move(board, color, depth)
-    print("Score: %d" % best_score)
+    # Retrieve the best move
+    best_move, _ = _find_best_move(board, color, depth)
 
     return best_move
-
-###############################################################################
-
-def random_play(board, color):
-    """
-        An example of play function based on allowed_moves.
-    """
-    moves = allowed_moves(board, color)
-    # There will always be an allowed move
-    # because otherwise the game is over and
-    # 'play' would not be called by main.py
-    return random.choice(moves)
